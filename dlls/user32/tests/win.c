152e15c02f16fddebd93fe88b00820e7ca6fefbe
Zebediah Figura <z.figura12@gmail.com>
2017-12-20 17:15:11 -0600

    user32: Use the C[XY]MINIMIZED rather than C[XY]ICON size for minimized windows.
    Signed-off-by: Zebediah Figura <z.figura12@gmail.com>

--- dlls/user32/tests/win.c
+++ dlls/user32/tests/win.c
@@ -6928,7 +6928,6 @@ static void test_ShowWindow(void)
     style = GetWindowLongA(hwnd, GWL_STYLE);
     ok(style & WS_MINIMIZE, "window should be minimized\n");
     GetWindowRect(hwnd, &rc);
-    todo_wine
     ok(EqualRect(&rcMinimized, &rc), "expected %s, got %s\n",
        wine_dbgstr_rect(&rcMinimized), wine_dbgstr_rect(&rc));
     GetClientRect(hwnd, &rc);
