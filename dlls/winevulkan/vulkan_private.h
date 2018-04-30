10f4786e9d51aa822846922c30104604e568adbf
Andrew Eikum <aeikum@codeweavers.com>
2018-04-30 09:40:13 -0500

    Revert "winevulkan: Get rid of unused "phys_dev" field from VkDevice_T."
    This reverts commit 9451a42cf72ba0ba62fb98340d467e028a5c5042.

--- dlls/winevulkan/vulkan_private.h
+++ dlls/winevulkan/vulkan_private.h
@@ -60,7 +60,7 @@ struct wine_vk_base
 struct VkCommandBuffer_T
 {
     struct wine_vk_base base;
-    struct VkDevice_T *device; /* parent */
+    VkDevice device; /* parent */
     VkCommandBuffer command_buffer; /* native command buffer */
 };
 
@@ -69,6 +69,7 @@ struct VkDevice_T
     struct wine_vk_base base;
     struct vulkan_device_funcs funcs;
     VkDevice device; /* native device */
+    struct VkPhysicalDevice_T *phys_dev; /* parent */
 
     struct VkQueue_T **queues;
     uint32_t max_queue_families;
@@ -104,7 +105,7 @@ struct VkPhysicalDevice_T
 struct VkQueue_T
 {
     struct wine_vk_base base;
-    struct VkDevice_T *device; /* parent */
+    VkDevice device; /* parent */
     VkQueue queue; /* native queue */
 };
 
