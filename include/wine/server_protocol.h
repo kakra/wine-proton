e7f6c42cb46ef114bbf951b5c07be9358d9d568b
Zebediah Figura <z.figura12@gmail.com>
2018-06-08 18:55:49 -0500

    server: Add a request to get the eventfd file descriptor associated with a waitable handle.

--- include/wine/server_protocol.h
+++ include/wine/server_protocol.h
@@ -5698,6 +5698,17 @@ struct create_esync_reply
 };
 
 
+struct get_esync_fd_request
+{
+    struct request_header __header;
+    obj_handle_t handle;
+};
+struct get_esync_fd_reply
+{
+    struct reply_header __header;
+};
+
+
 enum request
 {
     REQ_new_process,
@@ -5992,6 +6003,7 @@ enum request
     REQ_set_job_completion_port,
     REQ_terminate_job,
     REQ_create_esync,
+    REQ_get_esync_fd,
     REQ_NB_REQUESTS
 };
 
@@ -6291,6 +6303,7 @@ union generic_request
     struct set_job_completion_port_request set_job_completion_port_request;
     struct terminate_job_request terminate_job_request;
     struct create_esync_request create_esync_request;
+    struct get_esync_fd_request get_esync_fd_request;
 };
 union generic_reply
 {
@@ -6588,8 +6601,9 @@ union generic_reply
     struct set_job_completion_port_reply set_job_completion_port_reply;
     struct terminate_job_reply terminate_job_reply;
     struct create_esync_reply create_esync_reply;
+    struct get_esync_fd_reply get_esync_fd_reply;
 };
 
-#define SERVER_PROTOCOL_VERSION 569
+#define SERVER_PROTOCOL_VERSION 570
 
 #endif /* __WINE_WINE_SERVER_PROTOCOL_H */
