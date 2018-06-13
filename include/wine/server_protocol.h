c43645310e35efe793a5c9ccce649ede032d0fc7
Zebediah Figura <z.figura12@gmail.com>
2018-06-13 15:08:26 -0500

    server, ntdll: Pass the shared memory index back from get_esync_fd.
    This should let DuplicateHandle() work completely correctly for semaphores and mutexes.

--- include/wine/server_protocol.h
+++ include/wine/server_protocol.h
@@ -5731,7 +5731,7 @@ struct get_esync_fd_reply
 {
     struct reply_header __header;
     int          type;
-    char __pad_12[4];
+    unsigned int shm_idx;
 };
 
 enum esync_type
@@ -6644,6 +6644,6 @@ union generic_reply
     struct get_esync_fd_reply get_esync_fd_reply;
 };
 
-#define SERVER_PROTOCOL_VERSION 576
+#define SERVER_PROTOCOL_VERSION 577
 
 #endif /* __WINE_WINE_SERVER_PROTOCOL_H */
