6510863ab8009d4f39f0e8c1fe864f1dc8024fe4
Zebediah Figura <z.figura12@gmail.com>
2018-06-15 14:12:22 -0500

    server, ntdll: Implement alertable waits.
    We do this quite simply by waiting on an extra eventfd descriptor, which the server signals when a user APC is queued.

--- include/wine/server_protocol.h
+++ include/wine/server_protocol.h
@@ -5734,6 +5734,17 @@ struct get_esync_fd_reply
     unsigned int shm_idx;
 };
 
+
+struct get_esync_apc_fd_request
+{
+    struct request_header __header;
+    char __pad_12[4];
+};
+struct get_esync_apc_fd_reply
+{
+    struct reply_header __header;
+};
+
 enum esync_type
 {
     ESYNC_SEMAPHORE = 1,
@@ -6042,6 +6053,7 @@ enum request
     REQ_create_esync,
     REQ_open_esync,
     REQ_get_esync_fd,
+    REQ_get_esync_apc_fd,
     REQ_NB_REQUESTS
 };
 
@@ -6343,6 +6355,7 @@ union generic_request
     struct create_esync_request create_esync_request;
     struct open_esync_request open_esync_request;
     struct get_esync_fd_request get_esync_fd_request;
+    struct get_esync_apc_fd_request get_esync_apc_fd_request;
 };
 union generic_reply
 {
@@ -6642,8 +6655,9 @@ union generic_reply
     struct create_esync_reply create_esync_reply;
     struct open_esync_reply open_esync_reply;
     struct get_esync_fd_reply get_esync_fd_reply;
+    struct get_esync_apc_fd_reply get_esync_apc_fd_reply;
 };
 
-#define SERVER_PROTOCOL_VERSION 577
+#define SERVER_PROTOCOL_VERSION 578
 
 #endif /* __WINE_WINE_SERVER_PROTOCOL_H */
