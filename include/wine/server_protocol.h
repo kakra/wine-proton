370f2195048bdfa68ca83973b8f23c0324068b6c
Zebediah Figura <z.figura12@gmail.com>
2018-07-29 16:50:08 -0500

    ntdll, server: Initialize the shared memory portion on the server side.
    Simply using a CS only prevents this race within one process.

--- include/wine/server_protocol.h
+++ include/wine/server_protocol.h
@@ -5688,7 +5688,9 @@ struct create_esync_request
     unsigned int access;
     int          initval;
     int          type;
+    int          max;
     /* VARARG(objattr,object_attributes); */
+    char __pad_28[4];
 };
 struct create_esync_reply
 {
@@ -6656,6 +6658,6 @@ union generic_reply
     struct get_esync_apc_fd_reply get_esync_apc_fd_reply;
 };
 
-#define SERVER_PROTOCOL_VERSION 579
+#define SERVER_PROTOCOL_VERSION 580
 
 #endif /* __WINE_WINE_SERVER_PROTOCOL_H */
