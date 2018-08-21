c0cbe14aa894e87a7c6c06156c07bab796f49de6
Andrew Eikum <aeikum@codeweavers.com>
2018-08-21 14:47:33 -0500

    HACK: winex11: Work around mutter WM bug
    This would cause fullscreen windows to lose keyboard focus.

--- dlls/winex11.drv/event.c
+++ dlls/winex11.drv/event.c
@@ -792,8 +792,22 @@ static void focus_out( Display *display , HWND hwnd )
     Window focus_win;
     int revert;
     XIC xic;
+    struct x11drv_win_data *data;
 
     if (ximInComposeMode) return;
+
+    data = get_win_data(hwnd);
+    if(data){
+        if(data->take_focus_back){
+            data->take_focus_back = FALSE;
+            TRACE("workaround mutter bug, taking focus back\n");
+            XSetInputFocus( data->display, data->whole_window, RevertToParent, CurrentTime);
+            release_win_data(data);
+            /* don't inform win32 client */
+            return;
+        }
+        release_win_data(data);
+    }
 
     x11drv_thread_data()->last_focus = hwnd;
     if ((xic = X11DRV_get_ic( hwnd ))) XUnsetICFocus( xic );
@@ -1059,6 +1073,9 @@ static BOOL X11DRV_ConfigureNotify( HWND hwnd, XEvent *xev )
     }
 
     /* Get geometry */
+
+    /* fullscreening is done now, so stop avoiding focus loss */
+    data->take_focus_back = FALSE;
 
     parent = GetAncestor( hwnd, GA_PARENT );
     root_coords = event->send_event;  /* synthetic events are always in root coords */
