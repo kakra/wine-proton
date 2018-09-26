b1f01453a8a5a61edc2a24e245d103fe5416b8ac
Kai Krakow <kai@kaishome.de>
2018-09-26 20:22:42 +0200

    wineserver: Prefer SCHED_ISO over SCHED_FIFO
    Switching processes to SCHED_ISO scheduling provides very similar
    semantics to SCHED_FIFO without putting system stability at risk. It is
    also not a privileged operation and can be run by users without root
    privileges. So let's use SCHED_ISO by default unconditionally if
    supported, and fall-back to the previous implementation otherwise.
    
    As a consequence, this replaces priority implementation with nice levels
    in that case because SCHED_ISO doesn't know about different priorities.
    The user running such a setup needs to have RLIMIT_NICE raised to take
    advantage of this. With the new alternate implementation, only
    time-critical tasks will gain SCHED_ISO priority, other tasks will be
    reniced to different nice levels (0, -5, -10). Using nice levels also
    affects IO priorities of tasks, and as such has a secondary benefit to
    potential users of this patch.
    
    Signed-off-by: Kai Krakow <kai@kaishome.de>

--- server/scheduler.c
+++ server/scheduler.c
@@ -23,6 +23,7 @@
 
 #define _GNU_SOURCE  /* for SCHED_BATCH, SCHED_IDLE */
 #include <assert.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdarg.h>
 #ifdef HAVE_SYS_RESOURCE_H
@@ -33,6 +34,9 @@
 #endif
 #ifndef SCHED_RESET_ON_FORK
 # define SCHED_RESET_ON_FORK 0x40000000
+#endif
+#ifndef SCHED_ISO
+# define SCHED_ISO 4
 #endif
 #ifndef SCHED_IDLE
  #define SCHED_IDLE 5
@@ -46,6 +50,8 @@
 #if defined(__linux__) && defined(HAVE_SYS_RESOURCE_H) && defined(HAVE_SCHED_H)
 
 static int thread_base_priority = -1;
+static int use_sched_iso = 0;
+static int rlim_nice_max = 20; /* -1 = umlimited */
 
 /* gets the priority value from an environment variable */
 static int get_priority( const char *variable, int min, int max )
@@ -65,8 +71,48 @@ static int get_priority( const char *variable, int min, int max )
 /* initializes the scheduler */
 void init_scheduler( void )
 {
+    struct rlimit rlim;
+    struct sched_param param;
     int min, max, priority;
 
+    /* First try SCHED_ISO and use renice as a consequence for scheduling:
+     * SCHED_ISO is safe to use, thus we do not need to depend on an environment
+     * variable. If it succeeds, we prefer it over SCHED_FIFO and use nice
+     * priorities instead. SCHED_ISO provides realtime capabilities and low latency
+     * for processes without compromising system stability. Processes running under
+     * SCHED_ISO always run with highest priority and fall back to the nice value
+     * if their CPU usage stays above a certain threshold during the last 5 seconds
+     * averaged across all cores (/proc/sys/kernel/iso_cpu). This is supported by
+     * kernels implementing the MuQSS scheduler or its variants (i.e. PDS).
+     *
+     * Using nice also affects IO priorities in the best effort class:
+     * io_priority = (cpu_nice + 20) / 5
+     */
+    memset( &param, 0, sizeof(param) );
+    if (sched_setscheduler( 0, SCHED_ISO | SCHED_RESET_ON_FORK, &param ) == -1)
+    {
+        fprintf( stderr, "wineserver: SCHED_ISO not supported\n" );
+    }
+    else
+    {
+        /* rlim_nice = 20 - rlim_max */
+        getrlimit( RLIMIT_NICE, &rlim );
+        thread_base_priority = 20;
+        rlim_nice_max = rlim.rlim_max == -1 ? 40 : rlim.rlim_max;
+        if (debug_level) fprintf( stderr, "detected RLIMIT_NICE = %d, using SCHED_ISO\n", 20 - rlim_nice_max );
+
+        errno = 0;
+        if (setpriority( 0, PRIO_PROCESS, 20 - rlim_nice_max ) == -1)
+        {
+            fprintf( stderr, "wineserver: failed to change nice value to %d\n",
+                     20 - rlim_nice_max );
+        }
+
+        use_sched_iso = 1;
+        return;
+    }
+
+    /* Maybe fall back to SCHED_FIFO. */
     min = sched_get_priority_min( SCHED_FIFO );
     max = sched_get_priority_max( SCHED_FIFO );
     if (min == -1 || max == -1)
@@ -75,7 +121,6 @@ void init_scheduler( void )
     /* change the wineserver priority */
     if ((priority = get_priority( "STAGING_RT_PRIORITY_SERVER", min, max )) != -1)
     {
-        struct sched_param param;
         memset( &param, 0, sizeof(param) );
         param.sched_priority = priority;
         if (sched_setscheduler( 0, SCHED_FIFO | SCHED_RESET_ON_FORK, &param ) == -1 &&
@@ -106,6 +151,7 @@ void set_scheduler_priority( struct thread *thread )
 {
     struct sched_param param;
     int policy;
+    int nice = 20;
 
     if (thread_base_priority == -1) return;
     if (thread->unix_tid == -1) return;
@@ -113,30 +159,60 @@ void set_scheduler_priority( struct thread *thread )
     memset( &param, 0, sizeof(param) );
     if (thread->priority >= THREAD_PRIORITY_TIME_CRITICAL)
     {
-        policy = SCHED_FIFO;
-        param.sched_priority = thread_base_priority + 4;
+        if (use_sched_iso == 1)
+        {
+            policy = SCHED_ISO;
+            nice = rlim_nice_max;
+        }
+        else
+        {
+            policy = SCHED_FIFO;
+            param.sched_priority = thread_base_priority + 4;
+        }
     }
     else if (thread->priority >= THREAD_PRIORITY_HIGHEST)
     {
-        policy = SCHED_FIFO;
-        param.sched_priority = thread_base_priority + 2;
+        if (use_sched_iso == 1)
+        {
+            policy = SCHED_OTHER;
+            nice = thread_base_priority + 10;
+        }
+        else
+        {
+            policy = SCHED_FIFO;
+            param.sched_priority = thread_base_priority + 2;
+        }
     }
     else if (thread->priority >= THREAD_PRIORITY_ABOVE_NORMAL)
     {
-        policy = SCHED_FIFO;
-        param.sched_priority = thread_base_priority;
+        if (use_sched_iso == 1)
+        {
+            policy = SCHED_OTHER;
+            nice = thread_base_priority + 5;
+        }
+        else
+        {
+            policy = SCHED_FIFO;
+            param.sched_priority = thread_base_priority;
+        }
     }
     else if (thread->priority >= THREAD_PRIORITY_NORMAL)
     {
         policy = SCHED_OTHER;
+        if (use_sched_iso == 1)
+            nice = thread_base_priority;
     }
     else if (thread->priority >= THREAD_PRIORITY_LOWEST)
     {
         policy = SCHED_BATCH;
+        if (use_sched_iso == 1)
+            nice = thread_base_priority - 5;
     }
     else
     {
         policy = SCHED_IDLE;
+        if (use_sched_iso == 1)
+            nice = thread_base_priority - 10;
     }
 
     if (sched_setscheduler(thread->unix_tid, policy | SCHED_RESET_ON_FORK, &param) == -1 &&
@@ -151,6 +227,23 @@ void set_scheduler_priority( struct thread *thread )
 
     if (debug_level) fprintf( stderr, "%04x: changed priority to %d/%d\n",
                               thread->id, policy, param.sched_priority );
+
+    if (use_sched_iso == 1)
+    {
+        errno = 0;
+        nice = min( -20, 20 - min( rlim_nice_max, nice )); /* convert nice value from kernel to glibc interface */
+
+        if (setpriority( thread->unix_tid, PRIO_PROCESS, nice ) == -1)
+        {
+            static int once;
+            if (debug_level || !once++)
+                fprintf( stderr, "%04x: failed to change nice value to %d\n",
+                         thread->id, nice );
+        }
+
+        if (debug_level) fprintf( stderr, "%04x: changed nice value to %d\n",
+                                  thread->id, getpriority( thread->unix_tid, PRIO_PROCESS ));
+    }
 }
 
 #else
