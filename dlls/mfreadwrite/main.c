jzengc/wine

mfreadwrite: Reimplement MFCreateSourceReaderFromByteStream() with IMFSourceResolver.
Hack.

dlls/mfreadwrite/main.c
@@ -26,6 +26,7 @@
 #include "winbase.h"
 #include "initguid.h"
 #include "mfreadwrite.h"
+#include "mfidl.h"
 
 #include "wine/debug.h"
 
@@ -199,19 +200,17 @@ struct IMFSourceReaderVtbl srcreader_vtbl =
 
 HRESULT WINAPI MFCreateSourceReaderFromByteStream(IMFByteStream *stream, IMFAttributes *attributes, IMFSourceReader **reader)
 {
-    srcreader *object;
+    IMFSourceResolver *resolver = NULL;
+    MF_OBJECT_TYPE object_type;
+    HRESULT hr;
 
     TRACE("%p, %p, %p\n", stream, attributes, reader);
 
-    object = HeapAlloc( GetProcessHeap(), 0, sizeof(*object) );
-    if(!object)
-        return E_OUTOFMEMORY;
-
-    object->ref = 1;
-    object->IMFSourceReader_iface.lpVtbl = &srcreader_vtbl;
-
-    *reader = &object->IMFSourceReader_iface;
-    return S_OK;
+    MFCreateSourceResolver(&resolver);
+    hr = IMFSourceResolver_CreateObjectFromByteStream(resolver, stream, NULL, MF_RESOLUTION_MEDIASOURCE,
+                                                      NULL, &object_type, (IUnknown **)reader);
+    IMFSourceResolver_Release(resolver);
+    return hr;
 }
 
 HRESULT WINAPI MFCreateSourceReaderFromMediaSource(IMFMediaSource *source, IMFAttributes *attributes,

kakra/wine-proton

dlls/mfreadwrite/main.c
@@ -255,9 +255,15 @@ static HRESULT create_source_reader_from_source(IMFMediaSource *source, IMFAttri
 static HRESULT create_source_reader_from_stream(IMFByteStream *stream, IMFAttributes *attributes,
         REFIID riid, void **out)
 {
-    /* FIXME: resolve bytestream to media source */
+    IMFSourceResolver *resolver = NULL;
+    MF_OBJECT_TYPE object_type;
+    HRESULT hr;
 
-    return create_source_reader_from_source(NULL, attributes, riid, out);
+    MFCreateSourceResolver(&resolver);
+    hr = IMFSourceResolver_CreateObjectFromByteStream(resolver, stream, NULL, MF_RESOLUTION_MEDIASOURCE,
+                                                      NULL, &object_type, (IUnknown **)out);
+    IMFSourceResolver_Release(resolver);
+    return hr;
 }
 
 static HRESULT WINAPI sink_writer_QueryInterface(IMFSinkWriter *iface, REFIID riid, void **out)